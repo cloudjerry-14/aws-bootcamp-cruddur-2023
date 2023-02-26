dd0d6c65f4f82e456fb294b56f43a51f60a4075a
Ashutosh Parihar <ashutoshparihar0@gmail.com>
2023-02-26 17:20:18 +0000

    week1-notification-page

---
+++ backend-flask/services/notifications_activities.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta, timezone
+class NotificationsActivities:
+  def run():
+    now = datetime.now(timezone.utc).astimezone()
+    results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'Ashutosh Parihar',
+      'message': 'I am a Brown munda(guy) from India',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 5,
+      'replies_count': 1,
+      'reposts_count': 0,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'AndrewBrown',
+        'message': 'Hey Ashutosh that is really funny, Did not knew my lastname means Guy in India!',
+        'likes_count': 0,
+        'replies_count': 0,
+        'reposts_count': 0,
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }],
+    }]
+
+    return results
